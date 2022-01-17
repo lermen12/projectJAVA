70543c4c4e8df9357c5674a0d0a46697f12db3ff
lermen12 <lermenlucas@outlook.com>
2022-01-17 16:01:16 -0300

    Add new class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,10 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) throws Exception {
@@ -14,6 +16,9 @@ public class Program {
         } else {
             System.out.println("Erro de conexão");
         }
-
+        Department department = new Department(1,"books");
+        System.out.println(department);
+        Seller seller = new Seller(21,"Lucas","menorlermen@gmail.com",new Date(),3000.0,department);
+        System.out.println(seller);
     }
 }
